1649e7d314b0b2a41b3fe5f838592e2c6898c0e6
Michael Tobie Nnaji <tobiennaji@gmail.com>
2022-03-24 17:36:32 -0400

    Done

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+const writeCards=(names,message)=>{
+    let messages = [];
+        for(let name in names){
+            messages.push(`Thank you, ${names[name]}, for the wonderful ${message} gift!`);
+        }
+    return messages;
+}
+
+
+const countDown=(number)=>{
+    while(number >= 0){
+        console.log(number);
+        number--;
+    }
+}
+
