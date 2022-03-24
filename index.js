f0e528c03b93a8e59d851939fadf214f1e718d90
Michael Tobie Nnaji <tobiennaji@gmail.com>
2022-03-24 17:35:33 -0400

    commit tree 8ed16cb20d514401940eec84a9a1f0efb66ac070

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+const writeCards=(names,message)=>{
+    let messages = [];
+        for(let name in names){
+            messages.push(`Thank you, ${names[name]}, for the wonderful ${message} gift!`);
+        }
+    return messages;
+}
+
+
+const countDown=(number)=>{
+    while(number >= 0){
+        console.log(number);
+        number--;
+    }
+}
